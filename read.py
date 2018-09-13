edf124572e8e8b8d3e134802f8cb1982855f09fb
gwitfeng <gwitfeng@gmail.com>
2018-09-13 22:58:20 +0800

    add word filter

--- read.py
+++ read.py
@@ -23,3 +23,11 @@ for d in data:
 print('一共有', len(new), '比留言長度小於 100 ') 
 print(new[0])
 print(new[1])
+
+good = []
+for d in data:
+    if 'good' in d:
+        good.append(d)
+print('一共有 ', len(good), '筆留言有說到 good ')
+
+print(good[0])
